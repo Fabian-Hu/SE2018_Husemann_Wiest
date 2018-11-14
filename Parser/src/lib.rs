a6a969db79e11c1c80b7d0b53f9a59ecb4eabeee
Fabian Husemann <fabian@husemann-web.de>
2018-11-14 23:19:01 +0100

    Eingabenfehlerbehandlung

--- Parser/src/lib.rs
+++ Parser/src/lib.rs
@@ -8,16 +8,29 @@ pub struct Attribut {
 pub struct ObjectHelper {
 	pub object: Object,
 	pub count: usize,
+	pub errorCount: usize,				
+	pub errorMsg: String,
+}
+
+pub struct FunctionHelper {
+	pub function: Function,
+	pub count: usize,
+	pub errorCount: usize,		
+	pub errorMsg: String,
 }
 
 pub struct AttributHelper {
 	pub attribut: Attribut,
 	pub count: usize,
+	pub errorCount: usize,			
+	pub errorMsg: String,
 }
 
 pub struct RelationHelper{
 	pub relation: RelationObject,
 	pub count: usize,
+	pub errorCount: usize,			
+	pub errorMsg: String,
 }
 
 pub struct RelationObject{
@@ -28,9 +41,16 @@ pub struct RelationObject{
 	pub to: String,
 }
 
+pub struct Function {
+	pub name: String,
+	pub parameter: String, 
+	pub returnValue: String, 
+}
+
 pub struct Object {
 	pub name: String,
 	pub attributes: Vec<Attribut>, 
+	pub functions: Vec<Function>, 
 }
 
 pub fn printRelation(rela: &RelationObject,objList: &Vec<Object>) {
@@ -38,15 +58,15 @@ pub fn printRelation(rela: &RelationObject,objList: &Vec<Object>) {
 	println!("Ralationtyp: {}",rela.typ);
 	
 	for obj in objList{
-		if obj.name == rela.from {
-			println!("\nFrom: ");
+		if obj.name.to_string() == rela.from.to_string() {
+			println!("From: ");
 			printObject(&obj);
 		}
 	}
 
 	for obj in objList{
-		if obj.name == rela.to {
-			println!("\nTo: ");
+		if obj.name.to_string() == rela.to.to_string() {
+			println!("To: ");
 			printObject(&obj);
 		}
 		
@@ -62,8 +82,14 @@ pub fn printObject(obj: &Object) {
 		println!("Attributtyp: {}",attr.typ);
 		println!("Attributvalue: {}",attr.value);
 	}
+	if obj.functions.len() != 0 {
+		println!("-------------------------------------");
+	}
+	for func in obj.functions.iter() {
+		println!("Funktionsname: {}",func.name);
+		println!("Parameter: {}",func.parameter);
+		println!("Return: {}",func.returnValue);
+	}
 	println!("");	
 }
 
-
-//test
--- Parser/src/main.rs
+++ Parser/src/main.rs
@@ -4,6 +4,73 @@ use std::io::prelude::*;
 use std::path::Path;
 mod lib;
 
+fn createFunction(line: &Vec<&str>,lineCount: &usize) -> lib::FunctionHelper {
+	let mut lineCount2 = *lineCount;
+	let mut funcName = "";
+	let mut funcParameter = "";
+	let mut funcReturn = "";
+
+	let mut funcErrorCount = 0;
+	let mut funcErrorMsg  = String::new();
+
+	while !line[lineCount2 as usize].contains("/Function") && funcErrorCount == 0 {	
+		lineCount2 = lineCount2 + 1;
+		if line[lineCount2].contains("Name:") {
+			for split in line[lineCount2].split(": "){
+				if !split.contains("Name") {
+					funcName = split;	
+				}
+			}				  
+		}
+		else if line[lineCount2].contains("Parameter:") {
+			for split in line[lineCount2].split(": "){
+				if !split.contains("Parameter") {
+					funcParameter = split;	
+				}
+			}				  
+		}	
+		else if line[lineCount2].contains("Return:") {
+			for split in line[lineCount2].split(": "){
+				if !split.contains("Return") {
+					funcReturn = split;	
+				}
+			}				  
+		}
+		else if line[lineCount2].contains("/Function"){}
+		else {
+			funcErrorMsg.push_str("Fehler in Funktion an der Zeile: ");
+			funcErrorMsg.push_str(&(lineCount2+1).to_string());
+			funcErrorMsg.push_str(" ");
+			funcErrorMsg.push_str(&line[lineCount2].to_string());
+			funcErrorCount = 1;
+		}					
+	}
+
+	if funcName == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Funktion hat keinen Namen!");	
+		funcErrorCount = 1;
+	}
+
+	let func = lib::Function{
+		name: funcName.to_string(),
+		parameter: funcParameter.to_string(),
+		returnValue: funcReturn.to_string(),
+	};
+
+	let functionHelp = lib::FunctionHelper{
+		function: func,
+		count: lineCount2,
+		errorCount: funcErrorCount,			
+		errorMsg: funcErrorMsg.to_string(),
+	
+	};
+
+	return functionHelp;
+}
+
+
 fn createRelation(line: &Vec<&str>,lineCount: &usize) -> lib::RelationHelper {
 	let mut lineCount2 = *lineCount;
 	let mut relaDescription = "";
@@ -11,7 +78,10 @@ fn createRelation(line: &Vec<&str>,lineCount: &usize) -> lib::RelationHelper {
 	let mut relaFrom = "";
 	let mut relaTo = "";
 
-	while !line[lineCount2 as usize].contains("/Relation") {
+	let mut funcErrorCount = 0;
+	let mut funcErrorMsg  = String::new();
+
+	while !line[lineCount2 as usize].contains("/Relation") && funcErrorCount == 0 {
 		lineCount2 = lineCount2 + 1;
 		if line[lineCount2].contains("Description:") {
 			for split in line[lineCount2].split(": "){
@@ -41,6 +111,35 @@ fn createRelation(line: &Vec<&str>,lineCount: &usize) -> lib::RelationHelper {
 				}
 			}				  
 		}
+		else if line[lineCount2].contains("/Relation"){}
+		else {
+			funcErrorMsg.push_str("Fehler in Relation an der Zeile: ");
+			funcErrorMsg.push_str(&(lineCount2+1).to_string());
+			funcErrorMsg.push_str(" ");
+			funcErrorMsg.push_str(&line[lineCount2].to_string());
+			funcErrorCount = 1;
+		}
+	}
+
+	if relaTyp == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Relation hat keinen Typ: !");	
+		funcErrorCount = 1;
+	}
+
+	if relaFrom == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Relation hat keinen From: !");	
+		funcErrorCount = 1;
+	}
+
+	if relaTo == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Relation hat keinen To: !");	
+		funcErrorCount = 1;
 	}
 
 	let relationObject = lib::RelationObject {
@@ -54,6 +153,8 @@ fn createRelation(line: &Vec<&str>,lineCount: &usize) -> lib::RelationHelper {
 	let relationHelp = lib::RelationHelper{
 		relation: relationObject,
 		count: lineCount2,	
+		errorCount: funcErrorCount,			
+		errorMsg: funcErrorMsg.to_string(),
 	};
 
 	return relationHelp;
@@ -65,7 +166,10 @@ fn createAttribut(line: &Vec<&str>,lineCount: &usize) -> lib::AttributHelper {
 	let mut atttyp = "";
 	let mut attvalue = "";
 
-	while !line[lineCount2 as usize].contains("/Attribut") {
+	let mut funcErrorCount = 0;
+	let mut funcErrorMsg  = String::new();
+
+	while !line[lineCount2 as usize].contains("/Attribut") && funcErrorCount == 0 {
 		lineCount2 = lineCount2 + 1;
 		if line[lineCount2].contains("Name:") {
 			for split in line[lineCount2].split(": "){
@@ -87,32 +191,64 @@ fn createAttribut(line: &Vec<&str>,lineCount: &usize) -> lib::AttributHelper {
 					attvalue = split;	
 				}
 			}				  
+		}
+		else if line[lineCount2].contains("/Attribut"){}
+		else {
+			funcErrorMsg.push_str("Fehler in Attribut an der Zeile: ");
+			funcErrorMsg.push_str(&(lineCount2+1).to_string());
+			funcErrorMsg.push_str(" ");
+			funcErrorMsg.push_str(&line[lineCount2].to_string());
+			funcErrorCount = 1;
 		}	
+	}
+
+	if attname == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Attribut hat keinen Name: !");	
+		funcErrorCount = 1;
+	}
+
+	if atttyp == "" && funcErrorCount == 0{
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Attribut hat keinen Typ: !");	
+		funcErrorCount = 1;
 	}
 
 	let attr = lib::Attribut{
 		name: attname.to_string(),
 		typ: atttyp.to_string(),
-		value: atttyp.to_string(),
+		value: attvalue.to_string(),
 	};
 
 	let attributHelp = lib::AttributHelper{
 		attribut: attr,
 		count: lineCount2,	
+		errorCount: funcErrorCount,			
+		errorMsg: funcErrorMsg.to_string(),
 	};
 
 	return attributHelp;
 }
 
 fn createObject(line: &Vec<&str>,lineCount: &usize) -> lib::ObjectHelper {
-	let mut attrs = vec![];
+	let mut attrs : Vec<lib::Attribut> = vec![];
+	let mut funcs : Vec<lib::Function> = vec![];
 	let mut lineCount2 = *lineCount;
 	let mut objname = "";
 	
-	while line[lineCount2 as usize] != "/Object" {
+	let mut funcErrorCount = 0;
+	let mut funcErrorMsg  = String::new();
+
+	while line[lineCount2 as usize] != "/Object" && funcErrorCount == 0  {
 		lineCount2 = lineCount2 + 1;
 		if line[lineCount2].contains("Attribut") {
 			let attHelper = createAttribut(&line,&lineCount2);
+			if attHelper.errorCount != 0 {
+				print!("{}",attHelper.errorMsg);
+				funcErrorCount = 1;
+			}
 			attrs.push(attHelper.attribut);
 			lineCount2 = attHelper.count;
 		}
@@ -122,17 +258,45 @@ fn createObject(line: &Vec<&str>,lineCount: &usize) -> lib::ObjectHelper {
 					objname = split;	
 				}
 			}				  
-		}		
+		}	
+		else if line[lineCount2].contains("Function") {
+			let funcHelper = createFunction(&line,&lineCount2);
+			if funcHelper.errorCount != 0 {
+				print!("{}",funcHelper.errorMsg);
+				funcErrorCount = 1;
+			}
+			funcs.push(funcHelper.function);
+			lineCount2 = funcHelper.count;	
+									  
+		}
+		else if line[lineCount2].contains("/Object"){}
+		else {
+			funcErrorMsg.push_str("Fehler in Object an der Zeile: ");
+			funcErrorMsg.push_str(&(lineCount2+1).to_string());
+			funcErrorMsg.push_str(" ");
+			funcErrorMsg.push_str(&line[lineCount2].to_string());
+			funcErrorCount = 1;
+		}	
+	}
+
+	if objname == "" && funcErrorCount == 0 {
+		funcErrorMsg.push_str("Zeile: ");
+		funcErrorMsg.push_str(&lineCount2.to_string());
+ 		funcErrorMsg.push_str(" Fehler: Das Objekt hat keinen Name: !");	
+		funcErrorCount = 1;
 	}
 
 	let obj = lib::Object{
 		name: objname.to_string(),
 		attributes: attrs,
+		functions: funcs,
 	};
 
 	let objectHelp = lib::ObjectHelper{
 		object: obj,
 		count: lineCount2,	
+		errorCount: funcErrorCount,			
+		errorMsg: funcErrorMsg.to_string(),
 	};
 
 	return objectHelp;
@@ -145,7 +309,7 @@ fn main() {
 	*/
 
     // Create a path to the desired file
-    let path = Path::new("hello.txt");
+    let path = Path::new("Object.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -182,23 +346,43 @@ fn main() {
 	
 	let mut objList: Vec<lib::Object> = vec![];
 	let mut relaList: Vec<lib::RelationObject> = vec![];
+	let mut errorCounter = 0;
 
-    while lineCount < strings.len()  {
+    while lineCount < strings.len() && errorCounter == 0  {
 		if strings[lineCount].contains("Object") {
-			let obj = createObject(&strings,&lineCount);
+			let objHelper = createObject(&strings,&lineCount);
+			if objHelper.errorCount != 0 {
+				print!("{}",objHelper.errorMsg);
+				errorCounter = 1;
+			}
 			println!("");
-			objList.push(obj.object);
-			lineCount = obj.count;
+			objList.push(objHelper.object);
+			lineCount = objHelper.count;
 		}
 		else if strings[lineCount].contains("Relation") {
 			let relaHelper = createRelation(&strings,&lineCount);
+			if relaHelper.errorCount != 0 {
+				print!("{}",relaHelper.errorMsg);
+				errorCounter = 1;
+			}
 			relaList.push(relaHelper.relation);
 			lineCount = relaHelper.count;
 		}
+		else {
+			print!("Fehler in der Datei an der Zeile: ");
+			print!("{} ",&(lineCount+1).to_string());
+			println!("{} ",&strings[lineCount].to_string());
+			errorCounter = 1;
+		}	
+		
 		lineCount = lineCount + 1;
     }
-
-	for rela in relaList {
-		lib::printRelation(&rela, &objList);
+	if errorCounter == 0 {
+		for obj in &objList {
+			lib::printObject(&obj);
+		}
+		for rela in relaList {
+			lib::printRelation(&rela, &objList);
+		}
 	}
 }
